cfba23579b09c330d6a830e7a50f11a5f372214a
mihirsahu36 <123735395+mihirsahu36@users.noreply.github.com>
2024-02-15 00:17:21 +0530

    Day_1.3

---
+++ Variable_Swap.py
@@ -0,0 +1,9 @@
+a = input("a: ")
+b = input("b: ")
+print("Before Swap")
+print("Value of a: " + a + "\nValue of b: " + b)
+print("After Swap")
+temp = a
+a = b
+b = temp
+print("Value of a: " + a + "\nValue of b: " + b)
